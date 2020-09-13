91a8f9185d3a7635afd50eb6a9cc8282951d53b8
LoopGlitch26 <grandson.baba.2012@gmail.com>
2020-09-14 00:13:09 +0530

    Commit

--- basicFunc.jl
+++ basicFunc.jl
@@ -1,3 +1,4 @@
 typeof(10) 
 typemax(Int)
 typemin(Int)
+eps(1.4) # eps function is provided so that you can check, given a floating point value x, what is the distance to next larger value representable using the type of x
