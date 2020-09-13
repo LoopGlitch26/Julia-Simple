04c6058ac20f725fe2aded1c8b55bafb1760dab1
LoopGlitch26 <grandson.baba.2012@gmail.com>
2020-09-13 11:29:09 +0530

    Commit

--- funcShape.jl
+++ funcShape.jl
@@ -4,4 +4,5 @@ f(x) = x*sqrt(1-x^2)/2+sqrt(16*x^2-1)/16
 x = 0.25:0.0001:1.0
 using PyPlot
 pygui(true)
-plot(x, f.(x))
+plot(x, f.(x)) # Note that in order to apply the function f to all elements of the sequence x we appended . (a dot) to the function name. 
+maximum(f.(x))
