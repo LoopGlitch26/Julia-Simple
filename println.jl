b444e4ee7b5f26595ec289d6d9aa983e0012266f
LoopGlitch26 <grandson.baba.2012@gmail.com>
2020-09-14 19:47:36 +0530

    Commit

--- println.jl
+++ println.jl
@@ -1,1 +1,12 @@
 println("2-2.0","@#%^&! ",2+2) # separated by comma
+println(2+3)
+println(2. +3)
+println(2. +3.)
+println(typeof(2. +3))
+println(typeof(2+3))
+println(typeof(2. +3.))
+println(2+(2+3im))
+println(2. + (2 + 3im))
+println(Rational(2,3))
+println(Rational(2,3)+2)
+println(Rational(2,3)+2.0)
