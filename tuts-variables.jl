5cc408fa61e847011c6019197ba1a757345fe069
LoopGlitch26 <grandson.baba.2012@gmail.com>
2020-12-21 15:46:24 +0530

    Commit

---
+++ tuts-variables.jl
@@ -0,0 +1,38 @@
+# Variable
+#= Variable is a name
+bound to a value =#
+
+a = "Hello World" #always starts with alphabet or unicode
+α = raw"\alpha + tab"
+x,y,z = 2,3,4
+
+print("Hello World")
+println("Hello Julia")
+
+#= 
+
+julia> println("hello")
+hello
+
+julia> print("hello")
+hello
+julia> 
+
+=#
+
+show("shows everything")
+
+
+#Input
+readline()
+readline(STDIN) #v.0.6
+readline(stdin) #v.1.0
+
+#=
+
+julia> name = readline(stdin)
+gon
+"gon"
+
+=#
+
